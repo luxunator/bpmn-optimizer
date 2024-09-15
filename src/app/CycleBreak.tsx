8ae4a42293395d4065fba7732b04966e4fa75053
luxunator <luxuandthebox@gmail.com>
2024-09-15 14:56:54 -0600

    Add loop of cycle break steps until all vertices and edges are added/removed

--- src/app/CycleBreak.tsx
+++ src/app/CycleBreak.tsx
@@ -24,103 +24,107 @@ function CyleBreak(graph: mxGraph): void {
   const model = graph.getModel()
   const allVertices = model.filterDescendants((cell: mxCell) => { return cell.isVertex() }, model.getRoot());
 
-  // add the sink vertices and their incoming edges
-  let hasIncomingOnlyVertices = true;
-  while (hasIncomingOnlyVertices) {
-	const incomingOnlyVertices = allVertices.filter(cell => {
-      const outgoingEdges = model.getOutgoingEdges(cell).filter(edge => !model.getStyle(edge)?.includes('sequenceFlowAdded'));
-      const incomingEdges = model.getIncomingEdges(cell).filter(edge => !model.getStyle(edge)?.includes('sequenceFlowAdded'));
+  let hasVerticesAndEdgesLeft = true;
+  while (hasVerticesAndEdgesLeft) {
+	// add the sink vertices and their incoming edges
+	let hasIncomingOnlyVertices = true;
+	while (hasIncomingOnlyVertices) {
+	  const incomingOnlyVertices = allVertices.filter(cell => {
+		const outgoingEdges = model.getOutgoingEdges(cell).filter(edge => !model.getStyle(edge)?.includes('sequenceFlowAdded') && !model.getStyle(edge)?.includes('sequenceFlowRemoved'));
+		const incomingEdges = model.getIncomingEdges(cell).filter(edge => !model.getStyle(edge)?.includes('sequenceFlowAdded') && !model.getStyle(edge)?.includes('sequenceFlowRemoved'));
 
-	  return outgoingEdges.length === 0 && incomingEdges.length > 0 && !model.getStyle(cell)?.includes('vertexAdded');
-	});
-
-	const incomingOnlyVerticesEdges = incomingOnlyVertices.map(cell => {
-	  return model.getIncomingEdges(cell).filter(edge => !model.getStyle(edge)?.includes('sequenceFlowAdded'));
-	});
-
-	incomingOnlyVertices.forEach(cell => {
-	  model.setStyle(cell, 'vertexAdded;' + model.getStyle(cell));
-	});
-
-	incomingOnlyVerticesEdges.forEach(edge => {
-	  edge.forEach(cell => {
-	    model.setStyle(cell, 'sequenceFlowAdded');
+		return outgoingEdges.length === 0 && incomingEdges.length > 0 && !model.getStyle(cell)?.includes('vertexAdded');
 	  });
-	});
+
+	  const incomingOnlyVerticesEdges = incomingOnlyVertices.map(cell => {
+		return model.getIncomingEdges(cell).filter(edge => !model.getStyle(edge)?.includes('sequenceFlowAdded') && !model.getStyle(edge)?.includes('sequenceFlowRemoved'));
+	  });
+
+	  incomingOnlyVertices.forEach(cell => {
+		model.setStyle(cell, 'vertexAdded;' + model.getStyle(cell));
+	  });
+
+	  incomingOnlyVerticesEdges.forEach(edge => {
+		edge.forEach(cell => {
+	      model.setStyle(cell, 'sequenceFlowAdded');
+		});
+	  });
  
-	hasIncomingOnlyVertices = !(incomingOnlyVertices.length === 0 && incomingOnlyVerticesEdges.flat().length === 0);
-  }
+	  hasIncomingOnlyVertices = !(incomingOnlyVertices.length === 0 && incomingOnlyVerticesEdges.flat().length === 0);
+	}
 
-  // add the isolated vertices
-  const isolatedVertices = allVertices.filter(cell => {
-    const outgoingEdges = model.getOutgoingEdges(cell).filter(edge => !model.getStyle(edge)?.includes('sequenceFlowAdded'));
-	const incomingEdges = model.getIncomingEdges(cell).filter(edge => !model.getStyle(edge)?.includes('sequenceFlowAdded'));
+	// add the isolated vertices
+	const isolatedVertices = allVertices.filter(cell => {
+      const outgoingEdges = model.getOutgoingEdges(cell).filter(edge => !model.getStyle(edge)?.includes('sequenceFlowAdded') && !model.getStyle(edge)?.includes('sequenceFlowRemoved'));
+	  const incomingEdges = model.getIncomingEdges(cell).filter(edge => !model.getStyle(edge)?.includes('sequenceFlowAdded') && !model.getStyle(edge)?.includes('sequenceFlowRemoved'));
 
-	return outgoingEdges.length === 0 && incomingEdges.length === 0 && !model.getStyle(cell)?.includes('vertexAdded');
-  });
-
-  isolatedVertices.forEach(cell => {
-	model.setStyle(cell, 'vertexAdded;' + model.getStyle(cell));
-  });
-
-  // add the source vertices and their outgoing edges
-  let hasOutgoingOnlyVertices = true;
-  while (hasOutgoingOnlyVertices) {
-	const outgoingOnlyVertices = allVertices.filter(cell => {
-	  const outgoingEdges = model.getOutgoingEdges(cell).filter(edge => !model.getStyle(edge)?.includes('sequenceFlowAdded'));
-	  const incomingEdges = model.getIncomingEdges(cell).filter(edge => !model.getStyle(edge)?.includes('sequenceFlowAdded'));
-
-	  return outgoingEdges.length > 0 && incomingEdges.length === 0 && !model.getStyle(cell)?.includes('vertexAdded');
+	  return outgoingEdges.length === 0 && incomingEdges.length === 0 && !model.getStyle(cell)?.includes('vertexAdded');
 	});
 
-	const outgoingOnlyVerticesEdges = outgoingOnlyVertices.map(cell => {
-	  return model.getOutgoingEdges(cell).filter(edge => !model.getStyle(edge)?.includes('sequenceFlowAdded'));
-	});
-
-	outgoingOnlyVertices.forEach(cell => {
+	isolatedVertices.forEach(cell => {
 	  model.setStyle(cell, 'vertexAdded;' + model.getStyle(cell));
 	});
 
-	outgoingOnlyVerticesEdges.forEach(edge => {
-	  edge.forEach(cell => {
-		model.setStyle(cell, 'sequenceFlowAdded');
+	// add the source vertices and their outgoing edges
+	let hasOutgoingOnlyVertices = true;
+	while (hasOutgoingOnlyVertices) {
+	  const outgoingOnlyVertices = allVertices.filter(cell => {
+		const outgoingEdges = model.getOutgoingEdges(cell).filter(edge => !model.getStyle(edge)?.includes('sequenceFlowAdded') && !model.getStyle(edge)?.includes('sequenceFlowRemoved'));
+		const incomingEdges = model.getIncomingEdges(cell).filter(edge => !model.getStyle(edge)?.includes('sequenceFlowAdded') && !model.getStyle(edge)?.includes('sequenceFlowRemoved'));
+
+		return outgoingEdges.length > 0 && incomingEdges.length === 0 && !model.getStyle(cell)?.includes('vertexAdded');
 	  });
+
+	  const outgoingOnlyVerticesEdges = outgoingOnlyVertices.map(cell => {
+		return model.getOutgoingEdges(cell).filter(edge => !model.getStyle(edge)?.includes('sequenceFlowAdded') && !model.getStyle(edge)?.includes('sequenceFlowRemoved'));
+	  });
+
+	  outgoingOnlyVertices.forEach(cell => {
+		model.setStyle(cell, 'vertexAdded;' + model.getStyle(cell));
+	  });
+
+	  outgoingOnlyVerticesEdges.forEach(edge => {
+		edge.forEach(cell => {
+		  model.setStyle(cell, 'sequenceFlowAdded');
+	    });
+	  });
+
+	  hasOutgoingOnlyVertices = !(outgoingOnlyVertices.length === 0 && outgoingOnlyVerticesEdges.flat().length === 0);
+	}
+
+	// add the vertex with the most outgoing edges and least incoming edges
+	const vertices = allVertices.map(cell => {
+	  const outgoingEdges = model.getOutgoingEdges(cell).filter(edge => !model.getStyle(edge)?.includes('sequenceFlowAdded') && !model.getStyle(edge)?.includes('sequenceFlowRemoved'));
+	  const incomingEdges = model.getIncomingEdges(cell).filter(edge => !model.getStyle(edge)?.includes('sequenceFlowAdded') && !model.getStyle(edge)?.includes('sequenceFlowRemoved'));
+
+	  return { cell, outgoingEdges, incomingEdges };
 	});
 
-	hasOutgoingOnlyVertices = !(outgoingOnlyVertices.length === 0 && outgoingOnlyVerticesEdges.flat().length === 0);
-  }
+	const sortedVertices = vertices.sort((a, b) => {
+	  return (b.outgoingEdges.length - b.incomingEdges.length) - (a.outgoingEdges.length - a.incomingEdges.length);
+	});
 
-  // add the vertex with the most outgoing edges and least incoming edges
-  const vertices = allVertices.map(cell => {
-	const outgoingEdges = model.getOutgoingEdges(cell).filter(edge => !model.getStyle(edge)?.includes('sequenceFlowAdded'));
-	const incomingEdges = model.getIncomingEdges(cell).filter(edge => !model.getStyle(edge)?.includes('sequenceFlowAdded'));
+	if (sortedVertices.length > 0) {
+	  const vertex = sortedVertices[0];
 
-	return { cell, outgoingEdges, incomingEdges };
-  });
+	  if (vertex) {
+		model.setStyle(vertex.cell, 'vertexAdded;' + model.getStyle(vertex.cell));
 
-  const sortedVertices = vertices.sort((a, b) => {
-	const aOutgoing = a.outgoingEdges.length;
-	const aIncoming = a.incomingEdges.length;
-	const bOutgoing = b.outgoingEdges.length;
-	const bIncoming = b.incomingEdges.length;
+		vertex.outgoingEdges.forEach(cell => {
+		  model.setStyle(cell, 'sequenceFlowAdded');
+		});
 
-	return (bOutgoing - bIncoming) - (aOutgoing - aIncoming);
-  });
-
-  if (sortedVertices.length > 0) {
-	const vertex = sortedVertices[0];
-
-	if (vertex) {
-	  model.setStyle(vertex.cell, 'vertexAdded;' + model.getStyle(vertex.cell));
-
-	  vertex.outgoingEdges.forEach(cell => {
-		model.setStyle(cell, 'sequenceFlowAdded');
-	  });
-
-	  vertex.incomingEdges.forEach(cell => {
-		model.setStyle(cell, 'sequenceFlowRemoved');
-	  });
+		vertex.incomingEdges.forEach(cell => {
+		  model.setStyle(cell, 'sequenceFlowRemoved');
+		});
+	  }
 	}
+
+	hasVerticesAndEdgesLeft = allVertices.filter(cell => {
+		const outgoingEdges = model.getOutgoingEdges(cell).filter(edge => !model.getStyle(edge)?.includes('sequenceFlowAdded') && !model.getStyle(edge)?.includes('sequenceFlowRemoved'));
+		const incomingEdges = model.getIncomingEdges(cell).filter(edge => !model.getStyle(edge)?.includes('sequenceFlowAdded') && !model.getStyle(edge)?.includes('sequenceFlowRemoved'));
+		return outgoingEdges.length > 0 || incomingEdges.length > 0 || !model.getStyle(cell)?.includes('vertexAdded');
+	}).length > 0;
   }
 }
 
